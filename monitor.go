2f35a9571b827e36b723699391d3440eab1dc880
Pragmatic <cryptopragmatic@protonmail.com>
2023-01-09 16:28:52 +0100

    updates

--- monitor.go
+++ monitor.go
@@ -57,12 +57,11 @@ func (m *Monitor) loadBalances() {
 				logTelegram("Suspicious activity: " + a.Address)
 			}
 
-			// if !isNode(a.Address) {
-			// 	a.Balance = balance.Balance
-			// } else {
-			// 	a.Balance = 0
-			// }
-			a.Balance = balance.Balance
+			if !isNode(a.Address) {
+				a.Balance = balance.Balance
+			} else {
+				a.Balance = 0
+			}
 			db.Save(a)
 		}
 	}
